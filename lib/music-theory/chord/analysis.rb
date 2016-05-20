3404009cb9b7d4e912e14c54863803c041fc884e
Ari Russo <ari.russo@gmail.com>
2016-05-20 00:55:48 -0400

    triad report

--- lib/music-theory/chord/analysis.rb
+++ lib/music-theory/chord/analysis.rb
@@ -41,34 +41,12 @@ module MusicTheory
         [10, 11] => 3
       }.freeze
 
-      attr_reader :members
+      attr_reader :members, :root
 
       def initialize(*args)
         @members = args
-      end
-
-      # Get the root member of the chord
-      # @return [Note]
-      def root
-        if @root.nil?
-          members = abs_members.sort
-          root = nil
-          i = 0
-          while root.nil? && i < members.size
-            identifiers = TRIAD.values.map { |triad| triad[:intervals] }
-            if identifiers.any? { |identifier| identifier == members[0..identifier.size-1] }
-              root = members[0]
-            else
-              members.rotate!
-              i += 1
-            end
-          end
-          unless root.nil?
-            index = abs_members.index(root)
-            @root = @members[index]
-          end
-        end
-        @root
+        populate_triads
+        populate_root
       end
 
       # Get the name of the chord
@@ -81,23 +59,61 @@ module MusicTheory
       end
 
       def has_triad?(name)
-        triad = TRIAD[name.to_sym]
-        (triad[:intervals] & abs_members) == triad[:intervals]
+        @triads.key?(name)
       end
 
       def triad_names
-        TRIAD.keys.select { |name| has_triad?(name) }
+        @triad_names ||= @triads.keys
       end
 
       def inversion
-        key = INVERSION.keys.find { |k| k.include?(abs_members.first) }
+        key = INVERSION.keys.find { |k| k.include?(abs_notes(@members).first) }
         INVERSION[key]
       end
 
-      def abs_members
-        numbers = @members.map { |note| note.midi_note_num || note.interval_above_c }
+      private
+
+      def abs_notes(collection)
+        numbers = collection.map { |note| note.midi_note_num || note.interval_above_c }
         collapsed_members = Scale::Degree.collapse_all(numbers)
         Scale::Degree.normalize(collapsed_members)
+      end
+
+      def populate_triads
+        @triads = {}
+        TRIAD.keys.each do |name|
+          triad = TRIAD[name.to_sym]
+          abs = abs_notes(@members)
+          union = triad[:intervals] & abs
+          if union == triad[:intervals]
+            notes = union.map do |member|
+              index = abs.index(member)
+              @members[index]
+            end
+            @triads[name.to_sym] ||= []
+            @triads[name.to_sym] << notes
+          end
+        end
+        @triads
+      end
+
+      def populate_root
+        members = abs_notes(@members).sort
+        root = nil
+        i = 0
+        while root.nil? && i < members.size
+          identifiers = TRIAD.values.map { |triad| triad[:intervals] }
+          if identifiers.any? { |identifier| identifier == members[0..identifier.size-1] }
+            root = members[0]
+          else
+            members.rotate!
+            i += 1
+          end
+        end
+        unless root.nil?
+          index = abs_notes(@members).index(root)
+          @root = @members[index]
+        end
       end
 
     end
