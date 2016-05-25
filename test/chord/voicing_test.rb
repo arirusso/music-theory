4499e07b3811462c20987c40c69fde5d0daee9a8
Ari Russo <ari.russo@gmail.com>
2016-05-25 00:06:10 -0400

    not needed

--- test/chord/voicing_test.rb
+++ test/chord/voicing_test.rb
@@ -70,36 +70,5 @@ class MusicTheory::Chord::VoicingTest < Minitest::Test
 
     end
 
-    context ".get_inversion" do
-
-      should "recognize root inversion" do
-        assert_equal 0, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7], [0, 4, 7])
-        assert_equal 0, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7], [0, 7, 4])
-        assert_equal 0, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7], [0, 7, 4, 11])
-        assert_equal 0, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7], [0, 11, 7, 4])
-      end
-
-      should "recognize first inversion" do
-        assert_equal 1, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7], [4, 0, 7])
-        assert_equal 1, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7], [4, 7, 0])
-        assert_equal 1, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7], [4, 7, 0, 11])
-        assert_equal 1, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7], [4, 11, 7, 0])
-      end
-
-      should "recognize second inversion" do
-        assert_equal 2, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7], [7, 0, 4])
-        assert_equal 2, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7], [7, 4, 0])
-        assert_equal 2, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7], [7, 0, 4, 11])
-        assert_equal 2, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7], [7, 11, 4, 0])
-      end
-
-      should "recognize third inversion" do
-        assert_equal 3, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7, 11], [11, 7, 0, 4])
-        assert_equal 3, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7, 11], [11, 0, 7, 4])
-        assert_equal 3, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7, 11], [11, 4, 7, 0])
-        assert_equal 3, MusicTheory::Chord::Voicing.send(:get_inversion, [0, 4, 7, 11], [11, 4, 0, 7])
-      end
-
-    end
   end
 end
