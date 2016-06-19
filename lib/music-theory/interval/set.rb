d048ca16229fe998af8049fcad95cac4f02e90a3
Ari Russo <ari.russo@gmail.com>
2016-06-19 16:03:01 -0400

    extract permutation methods

--- lib/music-theory/interval/set.rb
+++ lib/music-theory/interval/set.rb
@@ -99,16 +99,26 @@ module MusicTheory
 
         private
 
-        def populate_permutations
+        def variations(permutations)
+          permutations.map { |set| Interval.reduce(set) } +
+            permutations.map { |set| Interval.normalize(set) }
+        end
+
+        def centered(intervals)
           permutations = []
-          (@set.members.count * 2).times do |i|
+          num_passes = intervals.count * 2
+          num_passes.times do |i|
             last = permutations.last
-            last = @set.members if last.nil? || i.zero?
+            last = intervals if last.nil? || i.zero?
             index = Interval.index_of_lowest(last, :rating => 2)
             permutations << Interval.center(last, :index => index)
           end
-          permutations += permutations.map { |set| Interval.reduce(set) }
-          permutations += permutations.map { |set| Interval.normalize(set) }
+          permutations
+        end
+
+        def populate_permutations
+          permutations = centered(@set.members)
+          permutations += variations(permutations)
           permutations.uniq!
           @permutations = permutations
         end
